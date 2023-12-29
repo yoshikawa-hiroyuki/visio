d702dd54576fdb3af21d9b0df24a04bae19d937c
Yoshikawa, Hiroyuki <yoh@fujitsu.com>
2023-12-29 18:21:37 +0900

    modify version

--- vsnApp.h
+++ vsnApp.h
@@ -29,14 +29,14 @@ namespace VSN {
   static const char vsn_progname[] = "Visio";
 
   // version code of the program
-  static const char vsn_version[] = "2.4.9a"
+  static const char vsn_version[] = "2.4.10"
 #ifdef PGSQL
     " (pgsql implements)"
 #endif // PGSQL
     ;
 
   // copyright of the program
-  static const char vsn_copyright[] = "(c) 2005-2015 RIKEN, 2018-2020 Fujitsu Ltd";
+  static const char vsn_copyright[] = "(c) 2005-2015 RIKEN, 2018-2023 Fujitsu Ltd";
 
 
   // symbols for wxMessageDialog
